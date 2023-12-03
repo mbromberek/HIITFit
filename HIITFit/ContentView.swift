6fe0e8da82744db8d852784379fb2e1cc08dd07f
Mike Bromberek <mikebromberek@gmail.com>
2023-12-03 09:21:32 -0600

    Setup Tab View with tabItem

--- HIITFit/ContentView.swift
+++ HIITFit/ContentView.swift
@@ -34,8 +34,17 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    Text("Hello, world!")
-      .padding()
+    TabView{
+      Text("Welcome!")
+        .tabItem { Text("Welcome") }
+      Text("Exercise 1")
+        .tabItem{ Text("Exercise 1") }
+      Text("Exercise 2")
+        .tabItem{ Text("Exercise 2") }
+    }
+        .tabViewStyle(PageTabViewStyle())
+        .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+    
   }
 }
 
