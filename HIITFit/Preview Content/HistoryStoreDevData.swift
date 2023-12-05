684a77586bd7c67d92e7b01d0f93fa59d2d9ea04
Mike Bromberek <mikebromberek@gmail.com>
2023-12-05 05:17:28 -0600

    Setup Data for when running in Debug/Dev for History

--- HIITFit/Preview Content/HistoryStoreDevData.swift
+++ HIITFit/Preview Content/HistoryStoreDevData.swift
@@ -1,15 +1,15 @@
 /// Copyright (c) 2023 Kodeco LLC
-///
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// This project and source code may use libraries or frameworks that are
 /// released under various Open-Source licenses. Use of those libraries and
 /// frameworks are governed by their own individual licenses.
@@ -31,19 +31,20 @@
 /// THE SOFTWARE.
 
 import Foundation
-struct ExerciseDay: Identifiable{
-  let id = UUID()
-  let date: Date
-  var exercises: [String] = []
-}
-struct HistoryStore{
-  var exerciseDays: [ExerciseDay] = []
-  
-  init(){
-    #if DEBUG
-    createDevData()
-    #endif
+
+extension HistoryStore{
+  mutating func createDevData(){
+    //Development data
+    exerciseDays = [
+      ExerciseDay(date: Date().addingTimeInterval(-86400), exercises: [
+        Exercise.exercises[0].exerciseName,
+        Exercise.exercises[1].exerciseName,
+        Exercise.exercises[2].exerciseName
+      ]),
+      ExerciseDay(date: Date().addingTimeInterval(-86400 * 2), exercises: [
+        Exercise.exercises[1].exerciseName,
+        Exercise.exercises[0].exerciseName
+      ])
+    ]
   }
 }
-
-
