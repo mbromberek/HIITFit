2d5a245a99fd1788fa4cd8ed27c4512c23465fe9
Mike Bromberek <mikebromberek@gmail.com>
2023-12-10 14:28:21 -0600

    Start work on EmbossedButtons

---
+++ HIITFit/Views/EmbossedButton.swift
@@ -0,0 +1,86 @@
+/// Copyright (c) 2023 Kodeco LLC
+/// 
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+/// 
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+/// 
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+/// 
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import SwiftUI
+
+/*
+struct EmbossedButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmbossedButton()
+}
+*/
+enum EmbossedButtonShape{
+  case round, capsule
+}
+struct EmbossedButtonStyle: ButtonStyle {
+  var buttonShape = EmbossedButtonShape.capsule
+  
+  func makeBody(configuration: Configuration) -> some View {
+    let shadow = Color("drop-shadow")
+    let highlight = Color("drop-highlight")
+    return configuration.label
+      .padding(10)
+      .background(
+        shape()
+          .foregroundColor(Color("background"))
+          .shadow(color: shadow, radius: 1, x: 2, y: 2)
+          .shadow(color: highlight, radius: 1, x: -2, y: -2)
+          .offset(x: -1, y: -1)
+      )
+  }
+  
+  func shape() -> some View{
+//    Capsule()
+    switch buttonShape{
+    case .round:
+//      Circle().stroke(Color("background"), lineWidth: 2)
+      return Capsule().stroke(Color("background"), lineWidth: 2)
+    case .capsule:
+      return Capsule().stroke(Color("background"), lineWidth: 2)
+    }
+  }
+}
+
+struct EmbossedButton_Previews: PreviewProvider {
+  static var previews: some View {
+    Button("History") {}
+      .fontWeight(.bold)
+      .buttonStyle(EmbossedButtonStyle())
+      .padding(40)
+      .previewLayout(.sizeThatFits)
+  }
+}
