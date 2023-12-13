03761463dbf0eb3767c07ae787047a36209398a9
Mike Bromberek <mikebromberek@gmail.com>
2023-12-13 16:53:07 -0600

    Setup chart to show last 7 days of exercises even if there are not workouts for a day

--- HIITFit/Views/History Views/BarChartWeekView.swift
+++ HIITFit/Views/History Views/BarChartWeekView.swift
@@ -1,15 +1,15 @@
 /// Copyright (c) 2023 Kodeco LLC
-/// 
+///
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-/// 
+///
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-/// 
+///
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-/// 
+///
 /// This project and source code may use libraries or frameworks that are
 /// released under various Open-Source licenses. Use of those libraries and
 /// frameworks are governed by their own individual licenses.
@@ -31,13 +31,30 @@
 /// THE SOFTWARE.
 
 import SwiftUI
+import Charts
 
 struct BarChartWeekView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  @EnvironmentObject var history: HistoryStore
+  @State private var weekData: [ExerciseDay] = []
+  
+  var body: some View {
+    Chart(weekData) { day in
+      BarMark(x: .value("Date", day.date, unit: .day), y: .value("Total Count", day.exercises.count))
     }
+      .onAppear{
+        let firstDate = history.exerciseDays.first?.date ?? Date()
+        let dates = firstDate.previousSevenDays
+        weekData = dates.map{ date in
+          history.exerciseDays.first(where: {$0.date.isSameDay(as: date) } ) ?? ExerciseDay(date: date)
+        }
+      }
+      .padding()
+  }
 }
 
-#Preview {
+struct BarChartWeekView_Previews: PreviewProvider{
+  static var previews: some View{
     BarChartWeekView()
+      .environmentObject(HistoryStore(preview: true))
+  }
 }
